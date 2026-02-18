00b4f6422ff7539de34280d15820c1fbed47d7c4
Stone59876 <cogo.clement@live.fr>
2026-02-18 13:05:48 +0100

    Initial commit

---
+++ src/main/java/com/clementcogo/mtgdeckassistant/MtgDeckAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.clementcogo.mtgdeckassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgDeckAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtgDeckAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clementcogo/mtgdeckassistant/MtgDeckAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clementcogo.mtgdeckassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgDeckAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
